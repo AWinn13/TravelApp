d2c46c668b1527a4580f46a10541ddb877c8b0b7
AWinn13 <anthonymichaelwinn@gmail.com>
2023-05-16 18:44:32 -0400

    another update

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -1,62 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Filters;
 using TravelApp.Models;
 namespace TravelApp.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
 
     private MyContext _context;
-    public TodoItemsController(MyContext context)
-    {
-        _context = context;
-    }
 
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public HomeController(ILogger<HomeController> logger, MyContext context)
     {
         _logger = logger;
+
+        _context = context;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<User>> PostTodoItem([FromBody] User newUser)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(User),
+                new { id = newUser.UserId },
+                newUser);
+        }
+        else
+        {
+            return BadRequest(ModelState);
+        }
     }
 
-    // [HttpGet]
-    // public IEnumerable<WeatherForecast> Get()
-    // {
-    //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    //     {
-    //         Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-    //         TemperatureC = Random.Shared.Next(-20, 55),
-    //         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    //     })
-    //     .ToArray();
-    // }
-
-
-    // !EXAMPLE  Create
-    // [HttpPost]
-    // public async Task<ActionResult<TodoItem>> PostTodoItem([FromBody] TodoItem todoItem)
-    // {
-    //     if (ModelState.IsValid)
-    //     {
-    //         _context.TodoItems.Add(todoItem);
-    //         await _context.SaveChangesAsync();
-    // This uses the GetTodoItem route we wrote above
-    //         return CreatedAtAction(
-    //             nameof(GetTodoItem),
-    //             new { id = todoItem.Id },
-    // todoItem);
-    //     }
-    //     else
-    //     {
-    // This is what will allow us to get error messages for our front end
-    //         return BadRequest(ModelState);
-    //     }
-    // }
 }
